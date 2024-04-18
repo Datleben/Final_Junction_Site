0ca9cdb0a1ddd86d0993696226d8764b5555ad58
Micah van Meurs <n01454650@unf.edu>
2024-04-17 22:10:21 -0400

    clean up test

--- Final_Junction_Site/Models/TestDBClass.cs
+++ Final_Junction_Site/Models/TestDBClass.cs
@@ -5,9 +5,5 @@
         public int TestDBClassId { get; set; }
         public string TestName { get; set; }
         public string TestDescription { get; set; }
-
-        public string TestDescription { get; set; }
-        //Thpublic string TestDescription { get; set; }is is a test of tpublic string TestDescription { get; set; }he push function
-        //ÄÄÄSSS public string TestDescription { get; set; }
     }
 }
