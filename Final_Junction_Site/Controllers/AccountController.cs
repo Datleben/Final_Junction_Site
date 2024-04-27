f3957b020b594601a8ef09ba3486616a7822583f
DragonsPants <wesleyjohnston007@gmail.com>
2024-04-26 21:24:34 -0400

    fixed account controller

--- Final_Junction_Site/Controllers/AccountController.cs
+++ Final_Junction_Site/Controllers/AccountController.cs
@@ -1,28 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Final_Junction_Site.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Final_Junction_Site.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Details(
-            new Customer{ SiteId = 1,
-            )
+        private Customer customer;
+        public AccountController()
+        {
+            customer = new Customer {
+                Customer_Id = 1,Customer_Name = "firsname lastname", Customer_Email = "cusomteremail@gmail.com",
+                Customer_Password = "Password", Customer_Address = "homeAddress", SendEmailNotifications = false, SendTextNotifications = true
+            };
+        }
+
+        public ViewResult Details()
         {
             return View();
         }
     }
 }
 
-Customer_Id { get; set; }
-
-        public string Customer_Name { get; set; }
-
-public string Customer_Email { get; set; }
-
-public string Customer_Password { get; set; }
-
-public string Customer_Address { get; set; }
-
-public bool SendEmailNotifications { get; set; }
-
-public bool SendTextNotifications { get; set; }
